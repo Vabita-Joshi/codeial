1df66aeaa9b89cb2ddede128d97308006b368bd4
Vabita joshi <vabita.joshi25@gmail.com>
2023-04-30 12:05:25 +0530

    Create dierctory structure and make first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { execPath } = require('process');
+const app = express();
+const port = 8000;
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`error in running the server ${err}`);
+    }
+    console.log(`Server is running on port ${port}`);
+})
