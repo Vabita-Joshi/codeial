ddd4cca8f91a9cb67ce934070305b44b4bf3ec40
Vabita joshi <vabita.joshi25@gmail.com>
2023-05-02 08:38:52 +0530

    adding more routes and controllers

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { execPath } = require('process');
 const app = express();
 const port = 8000;
+
+//use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
         console.log(`error in running the server ${err}`);
     }
     console.log(`Server is running on port ${port}`);
-})
+})  
